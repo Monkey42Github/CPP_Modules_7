9dea898cfcc662262a0383bcd279ba49cd0970a5
monkey <monkey@MacBook-Air-de-monkey.local>
2023-08-16 22:27:13 +0200

    C07

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -7,3 +7,9 @@ void    iter(T *tab, size_t size, void(*f)(T &))
         f(tab[i]);
 }
 
+template <typename T>
+void    iter(const T *tab, size_t size, void(*f)(const T &))
+{
+    for (size_t i = 0; i < size; i++)
+        f(tab[i]);
+}
